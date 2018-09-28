ec95a4c5071d9fd2e12994527928fc374af0923c
Talgorhythm <dia.99000@gmail.com>
2018-09-28 19:21:41 +0900

    implement bubble sort

--- sort/js/sortAlgorhythm.js
+++ sort/js/sortAlgorhythm.js
@@ -4,8 +4,11 @@ let app = new Vue({
 		arr: [],
 		number: null,
 		isAdded: false,
+		sortedArr: [],
+		isSorted: false,
 	},
 	methods: {
+		//入力を受け取って配列に格納
 		addNumber: function(event){
 			if(isNaN(parseInt(this.number, 10)) === false){
 				console.log(this.number);
@@ -14,5 +17,37 @@ let app = new Vue({
 			};
 			this.number = null;
 		},
+		putArr: function(){
+			this.sortedArr = this.arr;
+			this.isSorted = true;
+		},
+		bubbleSort: function(event){
+			this.putArr();
+			// console.log("bubbleSortよばれた");
+			for(let i = 0; i < this.sortedArr.length; i++){
+				console.log("1回目のfor文");
+				for(let j = this.sortedArr.length-1; j > i; j--){
+					if(this.sortedArr[j] < this.sortedArr[j-1]){
+						let tmp = this.sortedArr[j];
+						this.sortedArr[j] = this.sortedArr[j-1];
+						this.sortedArr[j-1] = tmp;
+						console.log(this.arr);
+
+					}
+				}
+			}
+		},
+		selectionSort: function(event){
+			this.putArr();
+		},
+		insertionSort: function(event){
+			this.putArr();
+		},
 	},
 })
+//バブルソート
+//選択ソート
+//挿入ソート
+//フープソート
+//マージソート
+//クイックソート
